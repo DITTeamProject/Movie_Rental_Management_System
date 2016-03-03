c0780959760708d21b50eeba9512eb349ae46a07
daniielf <daniielf.13@gmail.com>
2016-03-03 13:40:11 +0000

    Update search_bar_template.php

--- View/Index/search_bar_template.php
+++ View/Index/search_bar_template.php
@@ -2,7 +2,7 @@
 
 <div class="Search_Bar">
 	<form method="GET" action="index.php">
-		<input class="Search" type="text" name="search" placeholder="Search..." />
+		<input class="Search" type="text" name="search" placeholder="Search..." onkeydown=""/>
 		<input class="Button" type="Submit" value="Search" />
 	</form>
 </div>
