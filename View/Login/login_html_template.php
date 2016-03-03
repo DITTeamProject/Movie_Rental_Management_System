03981cca5d155d0fe7f1c97ff3fa7e1f8d3d8a3c
daniielf <daniielf.13@gmail.com>
2016-03-03 13:10:54 +0000

    Added files via upload

--- View/Login/login_html_template.php
+++ View/Login/login_html_template.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Login</title>
-		<link rel="stylesheet" type="text/css" href="Style/login_style.css">
+		<link rel="stylesheet" type="text/css" href="login_style.css">
 	</head>
 	<body>
 		<div class="Block_Top"></div>
@@ -9,21 +9,31 @@
 		<div class="Login">
 			<div class="Border">
 				<div class="Header">
-					<p class="Title">Movie Rental Manage System</p>
+					<p class="Title"><font color="#80bfff">M</font>ovie <font color="#80bfff">R</font>ental <br><font color="#80bfff">S</font>ystem</p>
 				</div>
 				<div class="Input">
 					<form>
 						<table>
-							<tr>
-								<td>Email: </td>
-								<td><input class="Text" type="text" name="username" /></td>
+							<tr>								
+								<td><input class="Text" type="text" name="username" placeholder=" Email"/></td>
 							</tr>
 							<tr>
-								<td>Password:</td>
-								<td><input class="Text" type="password" name="password" /></td>
+								<td><input class="Text" type="password" name="password" placeholder=" Password"/>
+							
+								
+								</td>
+							</tr>
+							<tr><td align="right"><font size="2px"><a href="google">Forgot Password?</a></font></td></tr>
+							<tr>
+								<td align="center">
+									<input class="Button" type="submit" value="Login" />
+									<input class="Button" type="submit" value="Register" />
+
+								</td>
+							<td>
 							</tr>
 						</table>
-						<input type="submit" value="Login" />
+						
 					</form>
 				</div>
 			</div>
