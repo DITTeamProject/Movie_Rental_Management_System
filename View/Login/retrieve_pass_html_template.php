be75294b2ceb17b74c9cc9081a81288c7b272cf9
daniielf <daniielf.13@gmail.com>
2016-03-07 15:28:48 +0000

    Added files via upload

---
+++ View/Login/retrieve_pass_html_template.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>MRS - Retrieve Password </title>
+	<link rel="stylesheet" type="text/css" href="retrieve_style.css">
+</head>
+<body align="center">	
+	<h1>Retrieve Password</h1>
+		<p>
+			<input class="Text" type="text" name="username" placeholder=" Email"/>
+	
+		<p>
+			<input class="Button" type="submit" value="Retrieve" />
+
+	
+</body>	
+</html>
