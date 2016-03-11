2e473e8b938b7ec784d6eecead8dc04fffa2d7e3
Ezyy <gytismack@gmail.com>
2016-03-11 15:33:11 +0000

    Modify configuration for database

--- Config/config.inc.php
+++ Config/config.inc.php
@@ -1,6 +1,9 @@
 <?php
 	define("DB_HOST", "localhost");										/*Database Host.*/
-	define("DB_USER", "test");											/*Database User Name.*/
+	define("DB_USER", "root");											/*Database User Name.*/
+	define("DB_CHARSET", "utf8" );
+	define("DB_PORT", 3306);
+	define("DB_DEBUGMODE",true);
 	define("DB_PASS", "");												/*Database Password.*/
 	define("DB_NAME", "movie_rental");									/*Database Name.*/
 	define("DB_TYPE", "mysqli");										/*Database Type.*/
