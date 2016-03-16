7022681fc797e41c754ce892b8d895282ca89928
jamesxu182 <jamesxu182@gmail.com>
2016-03-16 15:57:27 +0000

    Add Customer Class

---
+++ Class/Customer.php
@@ -0,0 +1,43 @@
+<?php
+	class Customer {
+		private $id = null;
+		private $username = null;
+		private $password = null;
+		private $email = null;
+		
+		public function __construct($id, $username, $password, $email) {
+			$this->id = $id;
+			$this->username = $username;
+			$this->password = $password;
+			$this->email = $email;
+		}
+		
+		public function setId($id) {
+			$this->id = $id;
+		}
+		
+		public function setUsername($username) {
+			$this->username = $username;
+		}
+		
+		public function setPassword($password) {
+			$this->password = $password;
+		}
+		
+		public function setEmail($email) {
+			$this->email = $email;
+		}
+		
+		public function getId() {
+			return $this->id;
+		}
+		
+		public function getUsername() {
+			return $this->username;
+		}
+		
+		public function getEmail() {
+			return $this->email;
+		}
+	}
+?>
