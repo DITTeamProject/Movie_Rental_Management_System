fe96dec7fa016692276692a49ace9f7268aee9ba
jamesxu182 <jamesxu182@gmail.com>
2016-03-16 16:20:05 +0000

    Add Customer DAO

---
+++ Database/DAO/CustomerDAO.php
@@ -0,0 +1,31 @@
+<?php
+	include_once('Class/Movie.php');
+	include_once('Database/DBManager.php');
+	
+	class CustomerDAO {
+		private $db_manager = null;
+		private $customer = null;
+		
+		public function __construct() {
+			$this->$db_manager = DBManager();
+			
+		}
+		
+		public function getCustomerByUserName($username) {
+			$this->db_manager.openConnection();
+			
+			$sql = "select * from Customer where User_Name = $username";
+			
+			$result = $this->db_manager.query();
+			
+			if($result->fetchInto($row)) {
+				$this->customer = new Customer($row[0], $row[1], $row[2], $row[3]);
+			}
+			
+			$this->db_manager.closeConnection();
+			
+			return $this->customer;
+		}
+	}
+	
+?>
