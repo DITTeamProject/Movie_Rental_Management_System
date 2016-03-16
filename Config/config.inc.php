866b9b2b2ab4c33696c23a950450e37a05a90d99
jamesxu182 <jamesxu182@gmail.com>
2016-03-16 15:53:42 +0000

    Add Customer Table Into Database

--- Config/config.inc.php
+++ Config/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 	define("DB_HOST", "localhost");										/*Database Host.*/
 	define("DB_USER", "root");											/*Database User Name.*/
-	define("DB_CHARSET", "utf8" );
-	define("DB_PORT", 3306);
-	define("DB_DEBUGMODE",true);
+	define("DB_CHARSET", "utf8" );										/*Database Charset*/
+	define("DB_PORT", 3306);											/*Database Port Number*/
+	define("DB_DEBUGMODE",true);										/*Database Debugging*/
 	define("DB_PASS", "");												/*Database Password.*/
 	define("DB_NAME", "movie_rental");									/*Database Name.*/
 	define("DB_TYPE", "mysqli");										/*Database Type.*/
