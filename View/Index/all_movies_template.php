f51f920f1b895c1ba966c868ffe486cda4939054
jamesxu182 <jamesxu182@gmail.com>
2016-03-26 16:56:35 +0000

    add hover feature

--- View/Index/all_movies_template.php
+++ View/Index/all_movies_template.php
@@ -5,10 +5,33 @@
 ?>
 		
 		<div class="Movie">
-			<img class="Preview" src="Cover/<?= $movie->getCover() ?>" alt="<?= $movie->getTitle() ?>" />
-			<h3><?= $movie->getTitle() ?></h3>
-		</div>
-		
+			<div class="Movie_Overview">
+				<img class="Preview" src="Cover/<?= $movie->getCover() ?>" alt="<?= $movie->getTitle() ?>" />
+				<h3><a class="Movie_Title"><?= $movie->getTitle() ?></a></h3>
+			</div>
+					
+			<div class="Popup">
+				<div class="Info_Cover">
+					<img src="Cover/<?= $movie->getCover() ?>" alt="<?= $movie->getTitle() ?>" />
+				</div>
+				<div class="Info_Message">
+					<table>
+						<tr>
+							<td>Title: </td>
+							<td><?= $movie->getTitle() ?></td>
+						</tr>
+						<tr>
+							<td>Genre: </td>
+							<td><?= $movie->getGenre() ?></td>
+						</tr>
+						<tr>
+							<td>Duration: </td>
+							<td><?= $movie->getDuration() ?></td>
+						</tr>
+					</table>
+				</div>
+			</div>	
+		</div>	
 <?php
 	}
 ?>
