26f5cac847da4199d5bad6b5a94cfd2d41ee0a8c
jamesxu182 <jamesxu182@gmail.com>
2016-04-19 13:17:34 +0100

    Fix more bugs

--- View/Help/side_bar_template.php
+++ View/Help/side_bar_template.php
@@ -1,24 +1,37 @@
 <!-- Slide bar module -->
 <!-- If user dosen't login, show Login and Sign up buttons. -->
-
-
+<?php
+	session_start();
+	if(!isset($_SESSION['customer'])) {
+?>
 <div class="Greeting">
-	<table>
-		<tr>
-			<td >
-				<font color="white">Hello, <?= $_SESSION['customer']->getUsername() ?>!</font>
-			</td>
-			<td >
-				<form method="POST" action="index.php">
-					<input type="submit" class="Side_Bar_Button" value="Log out" />
-					<input type="hidden" name="action" value="logout" />
-				</form>
-			</td>
-		</tr>
-	</table>
-</div>
 
-<div id="Side_Bar_Space"></div>
+					<form method="GET" action="login.php">
+						<input class="Side_Bar_Button" type="submit" value="LogIn / SignUp" />
+					</form>
+
+</div>	
+<!-- if user login, greet to user. -->
+<?php } else {?>
+	<div class="Greeting">
+		<table>
+			<tr>
+				<td >
+					<font color="white">Hello, <?= $_SESSION['customer']->getUsername() ?>!</font>
+				</td>
+				<td >
+					<form method="POST" action="index.php">
+						<input type="submit" class="Side_Bar_Button" value="Log out" />
+						<input type="hidden" name="action" value="logout" />
+					</form>
+				</td>
+			</tr>
+		</table>
+	</div>
+<?php }?>
+
+<div id="Side_Bar_Space">
+</div>
 
 <div id="Options">
 	<table>
@@ -28,10 +41,10 @@
 		<tr>		
 			<td align="center"><a href="./account.php">Account</a></td>
 		</tr>
-		<tr>			
+		<tr>
 			<td align="center"><a href="./transaction.php">Transaction</a></td>
 		</tr>
-		<tr>			
+		<tr>
 			<td align="center"><a href="./help.php">Help</a></td>
 		</tr>
 	</table>
